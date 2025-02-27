b745965f7fa26ee0e8fff91b1f5bdf2bce806065
Namrata Soni <namratasoni111@gmail.com>
2025-02-28 00:29:21 +0530

    Adds delimeter \n to split method Rspec -> returns the sum of numbers separated by \n

--- add.rb
+++ add.rb
@@ -1,7 +1,7 @@
 class Add
   def self.add(string_numbers)
     # Split the input string into an array of numbers
-    numbers = string_numbers.split(',').map(&:to_i)
+    numbers = string_numbers.split('\n').map(&:to_i)
 
     # Calculate the sum of the numbers
     sum = numbers.inject(0, :+)
